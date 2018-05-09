c161a911de43704b62f43655bbba8f156e3a9a4f
Veronica Kim <vero.a.kim@gmail.com>
2018-05-09 03:04:52 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -13,7 +13,7 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    artists.songs << artist.all.name
+    self.songs << artist.all.name
     # return an array of strings containing every musician's name
   end
 end
