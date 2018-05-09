30fecf15b68e8d413a861cb5ea2befef75976476
Veronica Kim <vero.a.kim@gmail.com>
2018-05-09 02:58:04 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,7 +7,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    self.artist.name
+    self.song 
     # when this method is called it should assign the song's artist to Drake
   end
 end
